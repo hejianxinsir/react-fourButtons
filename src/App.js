51f7448ce9f6b8989610deed44ef3f936f588713
hejianxin <hejianxinsir@gmail.com>
2020-05-03 20:37:38 +0800

    用 react-redux 库写的 demo

--- src/App.js
+++ src/App.js
@@ -1,34 +1,39 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux';
 
-class App extends React.Component {
-  constructor(){
-    super()
-  }
 
-  add1(){
-    this.props.onAdd1()
-  }
-  add2(){
-    this.props.onAdd2()
-  }
-  addif(){
-    this.props.onAddif()
-  }
-  addAfter(){
-    this.props.onAddAfter()
-  }
-
+class App extends Component {
   render(){
     return (
       <div className="App">
-        <div>你点击了{this.props.value}次</div>
-        <button onClick={()=>this.add1()}>+1</button>
-        <button onClick={()=>this.add2()}>+2</button>
-        <button onClick={()=>this.addif()}>单数则+1</button>
-        <button onClick={()=>this.addAfter()}>一秒后+1</button>
+        <div>你点击了{this.props.n}次</div>
+        <button onClick={()=>this.props.add1()}>+1</button>
+        <button onClick={()=>this.props.add2()}>+2</button>
+        <button>单数则+1</button>
+        <button>一秒后+1</button>
       </div>
     )
   }
 }
 
-export default App;
+// connect 返回一个新的函数，所以要加两个括号，分别给参数
+// 前面的部分叫做偏函数，这里的偏函数是：connect()
+function getPartialStore(state){
+  return {
+    n: state.n
+  }
+}
+
+const actionCreator = {
+  add1: ()=>{
+    console.log('add1')
+    return {type: 'add', payload: 1}
+  },
+  add2: ()=>{
+    console.log('add2')
+    return {type: 'add', payload: 2}
+  }
+}
+
+
+export default connect(getPartialStore,actionCreator)(App);
--- src/index.js
+++ src/index.js
@@ -4,13 +4,14 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 import {createStore} from 'redux';
+import { Provider } from 'react-redux';
 
 const stateChanger = (state, action)=>{
   if(state === undefined){
-    return 0
+    return {n: 0}
   }else{
     if(action.type === 'add'){
-      var newState = state + action.payload
+      var newState = {n: state.n + action.payload} 
       return newState
     }else{
       return state
@@ -20,36 +21,30 @@ const stateChanger = (state, action)=>{
 
 let store = createStore(stateChanger)
 
-render()
-store.subscribe(()=>{
-  render()
-})
-
-function addIfSingle(){
-  if(store.getState() % 2 === 1){
-    store.dispatch({type: 'add', payload: 1})
-  }
-}
-
-function addAfter(){
-  setTimeout(function(){
-    store.dispatch({type: 'add', payload: 1})
-  }, 1000)
-}
-
-function render(){
-  ReactDOM.render(
-    <React.StrictMode>
-      <App value={store.getState()}
-        onAdd1={()=>{store.dispatch({type:'add', payload: 1})}}
-        onAdd2={()=>{store.dispatch({type:'add', payload: 2})}}
-        onAddif={addIfSingle}
-        onAddAfter={addAfter}
-      />
-    </React.StrictMode>,
-    document.getElementById('root')
-  )
-}
-
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
 
 serviceWorker.unregister();
+
+
+
+// render()
+// store.subscribe(()=>{
+//   render()
+// })
+// function addIfSingle(){
+//   if(store.getState() % 2 === 1){
+//     store.dispatch({type: 'add', payload: 1})
+//   }
+// }
+// function addAfter(){
+//   setTimeout(function(){
+//     store.dispatch({type: 'add', payload: 1})
+//   }, 1000)
+// }
