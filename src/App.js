6bbe1be867164a7da44e750971176932aae60818
hejianxin <hejianxinsir@gmail.com>
2020-05-03 21:09:17 +0800

    添加解释笔记

--- src/App.js
+++ src/App.js
@@ -18,22 +18,33 @@ class App extends Component {
 
 // connect 返回一个新的函数，所以要加两个括号，分别给参数
 // 前面的部分叫做偏函数，这里的偏函数是：connect()
-function getPartialStore(state){
+
+// mapStateToProps 必须是一个函数，文档说的
+function mapStateToProps(state){
   return {
     n: state.n
   }
 }
 
-const actionCreator = {
-  add1: ()=>{
-    console.log('add1')
-    return {type: 'add', payload: 1}
-  },
-  add2: ()=>{
-    console.log('add2')
-    return {type: 'add', payload: 2}
+// mapDispatchToProps 可以是函数或对象
+// const mapDispatchToProps = {
+//   add1: ()=>{
+//     console.log('add1')
+//     return {type: 'add', payload: 1}
+//   },
+//   add2: ()=>{
+//     console.log('add2')
+//     return {type: 'add', payload: 2}
+//   }
+// }
+function mapDispatchToProps(dispatch){
+  return {
+    add1: ()=> dispatch({type: 'add', payload: 1}),
+    add2: ()=> dispatch({type: 'add', payload: 2})
   }
 }
 
+// connect 的作用是，把 mapStateToProps  mapDispatchToProps 合并
+// 起来作为 props 传给 App 组件。
+export default connect(mapStateToProps, mapDispatchToProps)(App);
 
-export default connect(getPartialStore,actionCreator)(App);
